e931476dcb405f52ba6d0b554899f5c87190d6bd
yammuni <132226028+yammuni@users.noreply.github.com>
2023-12-23 23:41:35 +0300

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WEBPLANE.Controllers
+{
+	[Authorize(Roles = "Admin")]
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
